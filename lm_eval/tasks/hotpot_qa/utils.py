ab6dff4cbcc31dd5704d5b7f649b3a9c2c0f09a8
smartliuhw <liuhwen@buaa.edu.cn>
2024-10-16 10:00:38 -0400

    hotpot_qa bug fixed

--- lm_eval/tasks/hotpot_qa/utils.py
+++ lm_eval/tasks/hotpot_qa/utils.py
@@ -51,7 +51,9 @@ def preprocess_function(examples):
         for i in range(context_len):
             title = context["title"][i]
             # text = "\n".join(context["sentences"][i])
-            sentences.extend(f"{i + 1}. {context["sentences"][i]}")
+            sentence = context["sentences"][i]
+            sentences.extend(f"{i + 1}. {sentence}")
+            # sentences.extend(f"{i + 1}. {context["sentences"][i]}")
             # facts.append(title + ":\n" + text)
         return "\n\n".join(random.sample(sentences, min(5, len(sentences))))
         
